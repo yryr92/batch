b3da10ca82fe860ebebdfa2180bc0af12a0a7254
yryr92 <rin217@naver.com>
2024-04-15 16:22:31 +0900

    movies 테이블 수정

--- sql/create_movies.sql
+++ sql/create_movies.sql
@@ -2,10 +2,11 @@ create table movies(
 	id int primary key,
     title varchar(50) not null,
     backdrop_path varchar(100) not null,
-    overview varchar(500) not null,
+    overview varchar(700) not null,
     popularity double,
     release_date date,
     video boolean,
     vote_average double,
-	vote_count int
+	vote_count int,
+    fileDownloadYn varchar(1) default 'N'
 );
